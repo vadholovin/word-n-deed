0f82ef45d39a6f9c448550037e4ff348e24acf5d
Vadym Holovin <holyvad@gmail.com>
2020-01-16 15:14:57 +0200

    Added mobile styles for index page

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,44 @@
 
 
 /**
+ * #HEADER SCROLL
+ */
+(function ($) {
+  $(window).scroll(function(){
+    let offsetY = $(window).scrollTop();
+
+    if ( offsetY > 100) {
+      $('.header').addClass('is-scrolled');
+      $('.o-main-content').addClass('is-header-scrolled');
+    } else if ( offsetY < 100) {
+      $('.header').removeClass('is-scrolled');
+      $('.o-main-content').removeClass('is-header-scrolled');
+    }
+  });
+})(jQuery);
+
+
+/**
+ * #MENU
+ */
+(function ($) {
+  $('.js-menu-toggler').click(function (e) { 
+    e.preventDefault();
+    $('.mobile-nav').toggleClass('is-open');
+    $('.page-mask').toggleClass('is-visible');
+    $('body').toggleClass('is-nav-mobile-open');
+  });
+
+  $('.js-mobile-nav-close, .js-page-mask').click(function (e) { 
+    e.preventDefault();
+    $('.mobile-nav').removeClass('is-open');
+    $('.page-mask').removeClass('is-visible');
+    $('body').removeClass('is-nav-mobile-open');
+  });
+})(jQuery);
+
+
+/**
  * #SLIDERS
  */
 (function ($) {
@@ -22,21 +60,12 @@
     dots: true,
     prevArrow: $('.js-top-banner-controls [class*=prev]'),
     nextArrow: $('.js-top-banner-controls [class*=next]'),
-    // responsive: [
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   },
-    // ]
   });
 
   // Clients slider
   $('.js-clients-slider').slick({
     infinite: true,
-    slidesToShow: 5,
+    slidesToShow: 2,
     slidesToScroll: 1,
     speed: 300,
     autoplay: true,
@@ -44,46 +73,74 @@
     mobileFirst: true,
     prevArrow: $('.js-clients-controls [class*=prev]'),
     nextArrow: $('.js-clients-controls [class*=next]'),
-    // responsive: [
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   },
-    // ]
+    responsive: [
+      {
+        breakpoint: 479,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
 
   // Team slider
   $('.js-team-slider').slick({
     infinite: true,
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 1,
     speed: 300,
     mobileFirst: true,
     prevArrow: $('.js-team-controls [class*=prev]'),
     nextArrow: $('.js-team-controls [class*=next]'),
-    // responsive: [
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1
-    //     }
-    //   },
-    // ]
+    responsive: [
+      {
+        breakpoint: 479,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
 
   $(window).on('resize', function() {
-    $('.top-banner-slider, .js-clients-slider, .js-team-slider').slick('resize');
+    $('.js-clients-slider, .js-team-slider').slick('resize');
 });
 
 })(jQuery);
 
 
 /**
- * #TOGGLE SEARCH FORM
+ * #SEARCH FORM
  */
 (function ($) {
   function hideSearch() {
